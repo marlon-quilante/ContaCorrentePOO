1236eb0ce5a0adf87ace8b3cfb2da592d6c5e482
Marlon Quilante <marlonquilante@gmail.com>
2025-04-22 21:15:52 -0300

    Add project files.

---
+++ ContaCorrentePOO/ContaCorrente.cs
@@ -0,0 +1,52 @@
+﻿namespace ContaCorrentePOO
+{
+    internal class ContaCorrente
+    {
+        public int numero = 0;
+        public double saldo = 0.0F;
+        public double limiteDebito = 0.0F;
+        public string[] registroMovimentacoes = new string[50];
+        public int qtdMovimentacoes = 0;
+
+        public void Saque(double valor, ContaCorrente conta)
+        {
+            saldo -= valor;
+            registroMovimentacoes[qtdMovimentacoes] = $"Saque na {conta}: R$ {valor.ToString("F")} | {DateTime.Now}";
+            qtdMovimentacoes++;
+        }
+
+        public bool SaquePermitido(double valor)
+        {
+            if (valor <= saldo + limiteDebito)
+                return true;
+            else
+            return false;
+        }
+
+        public void Deposito(double valor, ContaCorrente conta)
+        {
+            saldo += valor;
+            registroMovimentacoes[qtdMovimentacoes] = $"Depósito na {conta}: R$ {valor.ToString("F")} | {DateTime.Now}";
+            qtdMovimentacoes++;
+        }
+
+        public string ConsultaSaldo()
+        {
+            return $"Saldo atual: {saldo}";
+        }
+
+        public void TransferirPara(double valor, ContaCorrente conta)
+        {
+            saldo -= valor;
+            conta.saldo += valor;
+        }
+
+        public bool TransferenciaPermitida(double valor)
+        {
+            if (valor <= saldo)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ ContaCorrentePOO/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ContaCorrentePOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Controle de Conta Corrente");
+            Console.WriteLine("--------------------------");
+
+
+        }
+    }
+}
